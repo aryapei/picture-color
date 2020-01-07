c1740331b8174228b6c57699ca705893c26d6a1a
aryapei <13301038@bjtu.edu.cn>
2020-01-08 00:25:30 +0800

    补上上次提交自动忽略的build文件夹

---
+++ build/precache-manifest.3a47944a3feb8ae26ed2ec6df32aeb61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b678952d0e8403160cbe70365260344",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f3b73e2c710d22b70fe",
+    "url": "./static/css/main.ab60dc8a.chunk.css"
+  },
+  {
+    "revision": "2af135dbf65a6cd8e447",
+    "url": "./static/js/2.290012b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.290012b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f3b73e2c710d22b70fe",
+    "url": "./static/js/main.5b9a84c3.chunk.js"
+  },
+  {
+    "revision": "058d57bb0c81162d16e4",
+    "url": "./static/js/runtime-main.1569c185.js"
+  },
+  {
+    "revision": "0fa4b7734026da17390e4d088a354dc3",
+    "url": "./static/media/line.0fa4b773.jpg"
+  }
+]);
